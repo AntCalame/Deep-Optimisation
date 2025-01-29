bbbb0a5377ce24e03491ad619f8547df722bc96c
AntCalame <calame.antoine@gmail.com>
2025-01-29 18:57:36 +0100

    Update analyse_paper.py

--- HPC/analysis/analyse_paper.py
+++ HPC/analysis/analyse_paper.py
@@ -100,120 +100,25 @@ for  k in range(3):
     #plt.savefig(SSS+'\\bx.pdf', format='pdf', bbox_inches='tight')
     plt.show()
     
-"""
 
-fig, axes = plt.subplots(3, 3, figsize=(16, 6))
+plt.plot(1-do["100"]["fitness"]["max"]["t=all"]["global"]["mean"],
+         label="do",color="cornflowerblue")
+plt.plot(1-do_test["100"]["fitness"]["max"]["t=all"]["global"]["mean"][:,-1],
+         label="do_test",color="lightskyblue")
+plt.plot(1-do_retrain["100"]["fitness"]["max"]["t=all"]["global"]["mean"][:,-1],
+         label="do_ewc",color="blue")
 
-for i in range(3):
-    for j in range(3):
-        axes[i][j].boxplot(do[item[j]]["fitness"]["max"]["t=-1"]["global"]["distribution"][i][1:].reshape(-1),
-                           positions=[2], widths=0.4, patch_artist=True,
-                        boxprops=dict(facecolor=colors[0], color=colors[0], alpha=0.5),
-                        medianprops=dict(color='black'),
-                        whiskerprops=dict(color=colors[0]),
-                        capprops=dict(color=colors[0]),
-                        vert=False)
-        
-        
-        axes[i][j].boxplot(ewc[item[j]]["fitness"]["max"]["t=-1"]["global"]["distribution"][i][1:].reshape(-1),
-                           positions=[1], widths=0.4, patch_artist=True,
-                        boxprops=dict(facecolor=colors[1], color=colors[1], alpha=0.5),
-                        medianprops=dict(color='black'),
-                        whiskerprops=dict(color=colors[1]),
-                        capprops=dict(color=colors[1]),
-                        vert=False)
-        
-        for zz,z in enumerate(map_gomea.keys()):
-            axes[i][j].boxplot(gomea[z][item[j]]["fitness"]["max"]["global"]["distribution"][i].reshape(-1),
-                               positions=[3+zz], widths=0.4, patch_artist=True,
-                            boxprops=dict(facecolor=colors[2+zz], color=colors[2+zz], alpha=0.5),
-                            medianprops=dict(color='black'),
-                            whiskerprops=dict(color=colors[2+zz]),
-                            capprops=dict(color=colors[2+zz]),
-                            vert=False)
-        
+plt.plot(1-ewc["100"]["fitness"]["max"]["t=all"]["global"]["mean"],
+         label="ewc",color="red")
+plt.plot(1-ewc_test["100"]["fitness"]["max"]["t=all"]["global"]["mean"][:,-1],
+         label="ewc_test", color="orange")
+plt.plot(1-ewc_retrain["100"]["fitness"]["max"]["t=all"]["global"]["mean"][:,-1],
+         label="ewc_retrain", color="firebrick")
 
-        axes[i][j].set_yticks([])
-        axes[i][j].xaxis.set_major_locator(MaxNLocator(nbins=3))
-        
-        if j==0:
-            axes[i][j].set_ylabel(dim[i])
-        if i==2:
-            axes[i][j].set_xlabel(item[j])
-
-
-fig.suptitle(f"Fitness Ratios Boxplot per Dimension per Number of Item",fontsize=22)
-blue_patch = mpatches.Patch(color='blue', label='Replace-EWC', alpha=0.5)
-red_patch = mpatches.Patch(color='red', label='Base Algorithm', alpha=0.5)
-patchs = [blue_patch,red_patch]
-
-for p,(c,alg )in enumerate(map_gomea.items()):
-    patchs += [mpatches.Patch(color=colors[2+p], label="Gomea "+alg, alpha=0.5)]
-
-plt.tight_layout()
-fig.supxlabel("Number of item",y=-0.01)
-fig.supylabel("Dimension",x=-0.01)
-axes[0][0].legend(handles=patchs, loc='upper left', bbox_to_anchor=(0, 1.4), 
-           ncol=6, frameon=False)
-#plt.savefig(SSS+'\\bx.pdf', format='pdf', bbox_inches='tight')
+plt.yscale('log')
+plt.xlabel("transitions")
+plt.ylabel("1-fitness ratios")
+plt.title("Comparison Test/Retrain")
+plt.legend(ncol=2)
+plt.grid()
 plt.show()
-
-
-fig, axes = plt.subplots(2, 3, figsize=(16, 6))
-
-for i in range(2):
-    for j in range(3):
-        axes[i][j].boxplot(do[item[j]]["fitness"]["max"]["t=-1"]["global"]["distribution"][:, 1+i].reshape(-1),
-                           positions=[2], widths=0.4, patch_artist=True,
-                        boxprops=dict(facecolor=colors[0], color=colors[0], alpha=0.5),
-                        medianprops=dict(color='black'),
-                        whiskerprops=dict(color=colors[0]),
-                        capprops=dict(color=colors[0]),
-                        vert=False)
-        
-        
-        axes[i][j].boxplot(ewc[item[j]]["fitness"]["max"]["t=-1"]["global"]["distribution"][:, 1+i].reshape(-1),
-                           positions=[1], widths=0.4, patch_artist=True,
-                        boxprops=dict(facecolor=colors[1], color=colors[1], alpha=0.5),
-                        medianprops=dict(color='black'),
-                        whiskerprops=dict(color=colors[1]),
-                        capprops=dict(color=colors[1]),
-                        vert=False)
-        
-        for zz,z in enumerate(map_gomea.keys()):
-            axes[i][j].boxplot(gomea[z][item[j]]["fitness"]["max"]["global"]["distribution"][:, i].reshape(-1),
-                               positions=[3+zz], widths=0.4, patch_artist=True,
-                            boxprops=dict(facecolor=colors[2+zz], color=colors[2+zz], alpha=0.5),
-                            medianprops=dict(color='black'),
-                            whiskerprops=dict(color=colors[2+zz]),
-                            capprops=dict(color=colors[2+zz]),
-                            vert=False)
-        
-
-        axes[i][j].set_yticks([])
-        axes[i][j].xaxis.set_major_locator(MaxNLocator(nbins=3))
-        
-        if j==0:
-            axes[i][j].set_ylabel(tight[i+1])
-        if i==2:
-            axes[i][j].set_xlabel(item[j])
-
-
-fig.suptitle(f"Fitness Ratios Boxplot per Tightness Ratio per Number of Item",fontsize=22)
-blue_patch = mpatches.Patch(color='blue', label='Replace-EWC', alpha=0.5)
-red_patch = mpatches.Patch(color='red', label='Base Algorithm', alpha=0.5)
-patchs = [blue_patch,red_patch]
-
-for p,(c,alg )in enumerate(map_gomea.items()):
-    patchs += [mpatches.Patch(color=colors[2+p], label="Gomea "+alg, alpha=0.5)]
-
-plt.tight_layout()
-fig.supxlabel("Number of item",y=-0.01)
-fig.supylabel("Tightness",x=-0.01)
-axes[0][0].legend(handles=patchs, loc='upper left', bbox_to_anchor=(0, 1.2), 
-           ncol=6, frameon=False)
-#plt.savefig(SSS+'\\bx.pdf', format='pdf', bbox_inches='tight')
-plt.show()
-"""
-
-
